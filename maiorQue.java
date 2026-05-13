3d982eadd3bed08202d952f7e79c9873ceda422f
Diego Brito <dhba.dev@gmail.com>
2026-05-13 06:51:40 -0300

    Arquivo maiorQue indo para o repositório

---
+++ maiorQue.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class maiorQue{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Digite um número: ");
+        int n = sc.nextInt();
+        sc.nextLine(); 
+        
+        
+
+        double [] vet = new double[n] ; 
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Digite um número real (ex: 8,0) ");
+            vet[i] = sc.nextDouble();
+            
+        } 
+
+        double maior = 0;
+        double maiorValor = 0;
+
+        for(int i = 1; i < n; i++){
+            if (vet[i] > maior) {
+                maior = vet[i];
+                maiorValor = i;
+                
+            } 
+        }
+        
+        System.out.println("O maior valor é: " + maior);
+        System.out.println("Na posição " + maiorValor); 
+
+        sc.close();
+
+    }
+
+}
